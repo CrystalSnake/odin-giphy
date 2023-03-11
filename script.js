4f9bc7a21cdd407b97e1caefc5327d61527175e1
Crystal Snake <22963880+CrystalSnake@users.noreply.github.com>
2023-03-11 17:47:49 +0400

    refactor async/await

--- script.js
+++ script.js
@@ -4,21 +4,19 @@ const searchInput = document.querySelector('#search-input');
 const searchBtn = document.querySelector('#search-btn');
 let search = 'cats';
 
-function getGif() {
-  fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=twHfM7TghUB7C8bUD3ROP21PlgeCU7Pj&s=${search}`,
-    { mode: 'cors' }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
+async function getGif() {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=twHfM7TghUB7C8bUD3ROP21PlgeCU7Pj&s=${search}`,
+      { mode: 'cors' }
+    );
+    response.json().then(function (response) {
       img.src = response.data.images.original.url;
-    })
-    .catch(function (response) {
-      img.src = './not found.gif';
-      console.log(`Oops ${response}`);
     });
+  } catch (error) {
+    img.src = './not found.gif';
+    console.log(`Oops ${error}`);
+  }
 }
 
 btn.addEventListener('click', getGif);
